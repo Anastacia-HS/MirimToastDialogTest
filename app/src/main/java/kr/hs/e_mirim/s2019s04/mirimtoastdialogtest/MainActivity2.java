167d79739f204beb73cf40b2eb8acdf8edc182b3
Anastasia <kwon7788966@naver.com>
2020-11-17 19:56:54 +0900

    Complete Project Test1

---
+++ app/src/main/java/kr/hs/e_mirim/s2019s04/mirimtoastdialogtest/MainActivity2.java
@@ -0,0 +1,14 @@
+package kr.hs.e_mirim.s2019s04.mirimtoastdialogtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
